ffd7d407894de9645adb51db480dd92b85cff899
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-12-14 02:55:17 +0000

    tweak spicetify features descriptions

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,38 +4,23 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Themes',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    description: <>The customization you&apos;ve always needed, but didn&apos;t know you wanted.</>,
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Extensions',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    description: <>Small bonus little features that can be sprikled across Spotify.</>,
   },
   {
-    title: 'Powered by React',
+    title: 'Custom Apps',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    description: <>Whole new pages that complement Spotify&apos;s missing ones.</>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
