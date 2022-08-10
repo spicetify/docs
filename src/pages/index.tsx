931dbccc355294c19ccb5c5aad537bea58da2f37
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-08-10 13:34:56 +0100

    chore: update analytics source & updated deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Head from '@docusaurus/Head';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
@@ -34,13 +33,6 @@ function HomepageHeader() {
 export default function Home() {
   return (
     <Layout description="Powerful CLI tool to take control of the Spotify client.">
-      <Head>
-        <script
-          defer
-          data-domain-id="7bf4e4217ca9f5fa8bc3d53d183b9192a91d81f2"
-          src="https://fairdatacenter.de/cdn/fair.js"
-        />
-      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
